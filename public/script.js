92333cbccc04f3d57eecba7bcee271bb9080da7b
Emiliano <76189306+Emiliano9966@users.noreply.github.com>
2025-10-18 20:15:11 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 document.getElementById('goBtn').addEventListener('click', () => {
-  let url = document.getElementById('urlInput').value
-  if (!url) return alert("Enter a URL!")
+  let url = document.getElementById('urlInput').value;
+  if (!url) return alert("Enter a URL!");
 
   if (!/^https?:\/\//i.test(url)) {
-    url = 'https://' + url
+    url = 'https://' + url;
   }
 
-  window.open(`/api/proxy?url=${encodeURIComponent(url)}`, '_blank')
-})
+  window.open(`/api/proxy?url=${encodeURIComponent(url)}`, '_blank');
+});
